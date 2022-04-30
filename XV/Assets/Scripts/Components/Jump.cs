c2541cf70d7e8aab999f92222a13a69878abb8bb
Marie Chapard <mchapard@z3r7p1.42lyon.fr>
2022-04-30 19:20:06 +0200

    first person OK, third wip

---
+++ XV/Assets/Scripts/Components/Jump.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+    bool fly_check;
+    FirstPersonMovement fpm;
+
+    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+    GroundCheck groundCheck;
+
+
+    void Reset()
+    {
+        // Try to get groundCheck.
+        groundCheck = GetComponentInChildren<GroundCheck>();
+    }
+
+    void Awake()
+    {
+        // Get rigidbody.
+        rigidbody = GetComponent<Rigidbody>();
+        fpm = GetComponentInChildren<FirstPersonMovement>();
+
+    }
+
+    void LateUpdate()
+    {
+        fly_check = fpm.Is_flying;
+        // Jump when the Jump button is pressed and we are on the ground.
+        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded) && !fly_check)
+        {
+            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+            Jumped?.Invoke();
+        }
+    }
+}
