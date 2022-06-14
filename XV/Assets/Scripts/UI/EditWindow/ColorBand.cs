559d57394fb1472d2165e521f88a475c5c4eba25
Marie Chapard <mchapard@z4r7p7.42lyon.fr>
2022-06-14 20:14:35 +0200

    colors picker fix

--- XV/Assets/Scripts/UI/EditWindow/ColorBand.cs
+++ XV/Assets/Scripts/UI/EditWindow/ColorBand.cs
@@ -17,14 +17,23 @@ public class ColorBand : MonoBehaviour
 
     void CreatePicker()
     {
+        // GameObject previousColorPicker = FindObjectOfType<ColorPicker>()?.gameObject;
+        // if (previousColorPicker != null)
+        //     Destroy(previousColorPicker);
+        // RectTransform editWindowRT = (transform.root.GetChild(0) as RectTransform);
+        // GameObject pickerGO = Instantiate(m_ColorPicker, transform.root);
+        // ColorPicker picker = pickerGO.GetComponent<ColorPicker>();
+        // picker.CurrentColorIndex = int.Parse(gameObject.name);
+        // float xPos = (editWindowRT.sizeDelta.x / 2) + editWindowRT.anchoredPosition.x;
+        // (pickerGO.transform as RectTransform).anchoredPosition = new Vector2(xPos, editWindowRT.anchoredPosition.y);
         GameObject previousColorPicker = FindObjectOfType<ColorPicker>()?.gameObject;
         if (previousColorPicker != null)
             Destroy(previousColorPicker);
-        RectTransform editWindowRT = (transform.root.GetChild(0) as RectTransform);
-        GameObject pickerGO = Instantiate(m_ColorPicker, transform.root);
+        RectTransform editWindowRT = (transform.parent as RectTransform);
+        GameObject pickerGO = Instantiate(m_ColorPicker, transform.parent);
         ColorPicker picker = pickerGO.GetComponent<ColorPicker>();
         picker.CurrentColorIndex = int.Parse(gameObject.name);
-        float xPos = (editWindowRT.sizeDelta.x / 2) + editWindowRT.anchoredPosition.x;
+        float xPos = (editWindowRT.sizeDelta.x);
         (pickerGO.transform as RectTransform).anchoredPosition = new Vector2(xPos, editWindowRT.anchoredPosition.y);
     }
 }
